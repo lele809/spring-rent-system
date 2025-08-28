fcea4911818a9d8b4e7b8fcc21453726194c869f
lele809 <2998612110@qq.com>
2025-08-28 10:21:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: __dirname,
   server: {
     port: 3000,
     proxy: {
@@ -18,7 +20,10 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'esbuild'
+    minify: 'esbuild',
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
   },
   define: {
     __VUE_OPTIONS_API__: true,
